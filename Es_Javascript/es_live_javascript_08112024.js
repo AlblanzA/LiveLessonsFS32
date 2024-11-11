3732ed1f8a174b23a9f83d29cb2b7b150417c9fe
Campa01 <caniocampa01@gmail.com>
2024-11-11 09:42:41 +0100

    update_08112024

---
+++ Es_Javascript/es_live_javascript_08112024.js
@@ -0,0 +1,47 @@
+let nameUser = 'canio'
+let ageUser = 23
+let ageUser2= 10
+let isLogged = true
+let y= 5
+
+if (isLogged) {
+    console.log('sei loggato')
+
+//richiamo la funzione passando il parametro ageUser
+checkUserAge(ageUser)
+
+//funzione di test
+stampaMessaggio()
+
+//richiamo la funzione passando il parametro ageUser2
+checkUserAge(ageUser2)
+
+addNumber(y,ageUser)
+
+
+} else {
+    console.log('accedi per continuare')
+}
+
+//creo la funzione per verificare l'età dell'utente
+function checkUserAge (parametroPassato){
+    console.log("inizio controllo età")
+    if (parametroPassato >= 18) {
+        console.log('sei maggiorenne')
+    }
+    else {
+        console.log('sei minorenne')
+    }
+    console.log("fine controllo età")
+}
+
+function stampaMessaggio(){
+    console.log("funzione di test")
+}
+
+//creo una funzione per sommare 2 numeri
+function addNumber (y,ageUser) {
+let somma= y + ageUser
+console.log(somma)
+}
+
