be6a4eb9307db2bc7cbbee9266057eef1ad1de70
natasciarufino <natasciarufino17@gmail.com>
2024-11-20 11:29:25 +0100

    es_live_javascript_20112024.js

--- Es_Javascript/es_live_javascript_20112024.js
+++ Es_Javascript/es_live_javascript_20112024.js
@@ -12,3 +12,41 @@ switch (day) {
   default:
     console.log("undefined");
 }
+
+let ageUser = 20;
+if (ageUser > 18) {
+  console.log("sei maggiorenne")
+}
+else {
+  console.log("sei minorenne")
+}
+
+if (ageUser > 10 && ageUser < 17 ) {
+  console.log("sei un bambino")
+}
+else if (ageUser > 18) {
+  console.log("sei maggiorenne")
+} else {
+  console.log("sei un neonato")
+}
+
+let number = 10;
+for ( let y = 5; y <= number; y++) {
+console.log(y)
+}
+
+function message () {
+  console.log("stampa messaggio")
+}
+
+message()
+
+function sumNumbers(a, b, c) {
+  return a + b + c 
+}
+let results =  sumNumbers ( 13, 12, 15 )
+console.log(sumNumbers(3,2,5))
+console.log(results) 
+
+const days = ["lunedi", "martedi", "mercoledi", "giovedi"]
+console.log(days[1])
