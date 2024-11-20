3e15a80e6f3943470e6df7e272076fe921bf1b95
Campa01 <caniocampa01@gmail.com>
2024-11-20 10:31:07 +0100

    update

---
+++ Es_Javascript/es_live_javascript_20112024.js
@@ -0,0 +1,14 @@
+let day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("è lunedi");
+    break;
+
+  case "tuesday":
+    console.log("è martedi");
+    break;
+
+  default:
+    console.log("undefined");
+}
