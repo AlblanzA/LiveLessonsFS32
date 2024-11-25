360ed184337b822528f37dc5e34ab9e4375815f3
natasciarufino <natasciarufino17@gmail.com>
2024-11-25 11:18:30 +0100

    es_live_javascript25112024.js

--- Es_Javascript/es_live_javascript_25112024.js
+++ Es_Javascript/es_live_javascript_25112024.js
@@ -17,3 +17,65 @@ switch (user.name) {
   default:
     console.log("Undefined");
 }
+
+if (user.age > 18) {
+  console.log ("sei maggiorenne")
+}
+else {
+console.log("sei minorenne") 
+}
+
+
+if (user.age > 10 && user.age < 18) {
+  console.log("sei un bambino")
+}
+else if (user.age > 18) {
+  console.log("sei maggiorenne")
+}
+else {
+  console.log("sei un ninfante")
+} 
+
+const numbers = [ 1, 2 ,3, 4]
+
+function sum(a, b) {
+return a + b 
+}
+let result = sum(numbers[1], numbers[3])
+//console.log(sum(numbers[1], numbers[3])) 
+console.log(result / 2 )
+
+for (let i=0; i <= 10; i++){
+  console.log(i)
+}
+
+const object = [ 
+  { name: "canio",
+    age: 23,
+  },
+
+  {
+    name: "alberto",
+    age: 24,
+  }
+
+]
+
+console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+
+console.log("il mio nome è" + ' ' + object[1].name)
+for (let indice in object) {
+  console.log(object[indice].name)
+
+  //verifico ogni nome trovato se cè giovanni 
+  if (object[indice].name == "giovanni") {
+    console.log("c'è giovanni")
+  } else {
+    console.log("giovanni non è presente, lo inserisco")
+    // inserisco nell'array di oggetti l'oggetto giovanni 
+    object.push({name: "giovanni", age: 25})
+ console.log("inserimento avvenuto con successo")
+  }
+}
+console.log(object)
