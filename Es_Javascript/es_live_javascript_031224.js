b37db0001dc75be4a059376bbed9c58f3305fc29
Alberto Lanzafame <lanzafamea2001@gmail.com>
2024-12-03 12:09:44 +0100

    update

--- Es_Javascript/es_live_javascript_031224.js
+++ Es_Javascript/es_live_javascript_031224.js
@@ -1,45 +1,52 @@
-const item1 = { id: 1, brand: "apple", name: "iphone 18X", quantity: 100}
-const warehouse = []
+const item1 = { id: 1, brand: "apple", name: "iphone 18X", quantity: 100 };
+const warehouse = [];
 
-
-//crea una funzione che aggiunge elementi all'array 
-function add (arr, elements) {
-    arr.push(elements) 
-    return arr
+//INIZIO FUNZIONI
+//crea una funzione che aggiunge elementi all'array
+function add(arr, elements) {
+  arr.push(elements);
+  return arr;
 }
 
-const results = add(warehouse, item1)
+function checkAvailability(arr, qt) {
+  return arr.filter((element) => element.quantity >= qt);
+}
 
-const item2 = { id: 2, brand: "samsung", name: "galaxy S20", quantity: 150}
-add(warehouse, item2) 
-add(warehouse, {id: 3, brand: "lg", name: "televisione", quantity: 200})
+function printElements(arr) {
+  arr.forEach((element) => {
+    console.log("- nome: " + element.name + " , quantity: " + element.quantity);
+  });
+}
 
+function sum(quantity, somma) {
+  return quantity + somma;
+}
 
+//FINE FUNZIONI
 
-console.log("inizio controllo magazzino")
-console.log("elenco elementi contenuti nel magazzino: ")
+const results = add(warehouse, item1);
 
-let somma = 0 
+const item2 = { id: 2, brand: "samsung", name: "galaxy S20", quantity: 150 };
+add(warehouse, item2);
+add(warehouse, { id: 3, brand: "lg", name: "televisione", quantity: 200 });
 
+console.log("inizio controllo magazzino");
+console.log("elenco elementi contenuti nel magazzino: ");
+
+let somma = 0;
 
 // warehouse.forEach((element) => {
 //    console.log("- nome: " + element.name + " , quantity: " + element.quantity)
 // //   somma = somma + element.quantity
 // somma += element.quantity
 // })
-printElements(warehouse)
-console.log(checkAvailability(warehouse, 200))
-console.log("totale elementi contenuti nel magazzino: " + warehouse.length )
-console.log("quantità in magazzino: " + somma )
 
-function checkAvailability(arr, qt) {
-    return arr.filter(element => element.quantity >= qt )
+warehouse.forEach((element) => {
+  somma = sum(element.quantity, somma);
+});
+console.log(somma);
 
-}
-
-function printElements(arr) {
-    arr.forEach((element) => { 
-        console.log("- nome: " + element.name + " , quantity: " + element.quantity)
-
-    })
-}
+printElements(warehouse);
+console.log(checkAvailability(warehouse, 200));
+console.log("totale elementi contenuti nel magazzino: " + warehouse.length);
+console.log("quantità in magazzino: " + somma);
